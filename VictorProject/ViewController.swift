22a86cb3c94aeb3b94fc14a7f14cd0f1f2048a5c
João Pacheco <joaovictormacielpacheco@gmail.com>
2025-01-13 21:14:07 -0300

    Initial Commit

---
+++ VictorProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VictorProject
+//
+//  Created by Joao Pacheco on 13/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VictorProjectTests/VictorProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  VictorProjectTests.swift
+//  VictorProjectTests
+//
+//  Created by Joao Pacheco on 13/01/25.
+//
+
+import Testing
+@testable import VictorProject
+
+struct VictorProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
