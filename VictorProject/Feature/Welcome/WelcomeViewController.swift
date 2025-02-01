885680da99aa6af61dc1650fcd8e5e14dfb7ee58
João Pacheco <joaovictormacielpacheco@gmail.com>
2025-02-01 01:38:15 -0300

    Alterando algumas cores no app
    Fiz a alteração do fundo do app na primeira e segunda tela e efetuei a mudança da cor do botão back na segunda tela que quando pressionada faz a volta para a primeira tela.

--- VictorProject/Feature/Welcome/WelcomeViewController.swift
+++ VictorProject/Feature/Welcome/WelcomeViewController.swift
@@ -3,13 +3,13 @@ import SnapKit
 
 final class WelcomeViewController: UIViewController {
     // MARK: - UI Components
-    private let gradientLayer: CAGradientLayer = {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [UIColor.white.cgColor, UIColor.blue.cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0.2, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-        return gradientLayer
-    }()
+//    private let gradientLayer: CAGradientLayer = {
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.colors = [UIColor.white.cgColor, UIColor.blue.cgColor]
+//        gradientLayer.startPoint = CGPoint(x: 0.2, y: 0.0)
+//        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+//        return gradientLayer
+//    }()
     
     private var logoImageView: UIImageView = {
         let imageView = UIImageView()
@@ -53,26 +53,99 @@ final class WelcomeViewController: UIViewController {
         setupConstraints()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        gradientLayer.frame = view.bounds
-    }
     
     // MARK: - Setup
     private func viewSetup() {
-        let gradientView = UIView()
-        view.addSubview(gradientView)
+        //let gradientView = UIView()
+        //view.addSubview(gradientView)
         
-        gradientView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+//        gradientView.snp.makeConstraints { make in
+//            make.edges.equalToSuperview()
+//        }
+//        
+//        gradientView.layer.insertSublayer(gradientLayer, at: 0)
         
         view.addSubview(button)
         view.addSubview(textField)
         view.addSubview(logoImageView)
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Garantir que o fundo seja recriado sempre que o layout for atualizado
+        setupGradient()
+        setupHoneycombPattern()
+    }
+    
+    // MARK: - Setup Gradient
+    private func setupGradient() {
+        view.layer.sublayers?.removeAll { $0 is CAGradientLayer }
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.systemBlue.cgColor,
+            UIColor.systemPurple.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = view.bounds
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    // MARK: - Setup Honeycomb (Padrão de Colmeia)
+    private func setupHoneycombPattern() {
+        view.layer.sublayers?.removeAll { $0 is CAShapeLayer }
+        
+        let shapeLayer = CAShapeLayer()
+        let path = UIBezierPath()
+        
+        let hexSize: CGFloat = 20 // Tamanho dos hexágonos
+        let hexWidth = hexSize * 2
+        let hexHeight = sqrt(3) * hexSize
+        let spacing = hexSize * 0.2 // Pequeno espaço entre os hexágonos
+        
+        let width = view.bounds.width
+        let height = view.bounds.height
+        
+        for x in stride(from: 0, to: width, by: hexWidth + spacing) {
+            for y in stride(from: 0, to: height, by: hexHeight + spacing) {
+                
+                let offsetX = (Int(y / hexHeight) % 2 == 0) ? 0 : hexWidth / 2
+                
+                let centerX = x + offsetX
+                let centerY = y
+                
+                let hexagonPath = createHexagonPath(center: CGPoint(x: centerX, y: centerY), size: hexSize)
+                path.append(hexagonPath)
+            }
+        }
+        
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = UIColor.white.withAlphaComponent(0.15).cgColor // Apenas as linhas
+        shapeLayer.lineWidth = 1
+        shapeLayer.fillColor = UIColor.clear.cgColor // Remove qualquer preenchimento
+        
+        view.layer.insertSublayer(shapeLayer, above: view.layer.sublayers?.first)
+    }
+    
+    // Função auxiliar para desenhar um hexágono sem preenchimento
+    private func createHexagonPath(center: CGPoint, size: CGFloat) -> UIBezierPath {
+        let path = UIBezierPath()
+        for i in 0..<6 {
+            let angle = CGFloat(i) * (CGFloat.pi / 3) // 60 graus por lado
+            let x = center.x + size * cos(angle)
+            let y = center.y + size * sin(angle)
+            if i == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        path.close()
+        return path
+    }
+    
     
     private func setupConstraints() {
         button.snp.makeConstraints { make in
